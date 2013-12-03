f69a8e2607544f1b43ae2330aee653361c748915
pekka-m <propeke@gmail.com>
2013-12-03 16:32:49 +0200

    WIP1.2

--- harj22/harj22/paa.cpp
+++ harj22/harj22/paa.cpp
@@ -13,24 +13,24 @@ int main() {
 			cout << "\nHeidåå!\n\n";
 			break;
 		}
-		else if (valinta == 1) {
+		else if (valinta == 1) { //lisaa henkilo
 			LisaaHenkilo(henkilot, &henkilolkm);
 		}
-		else if (valinta == 2) {
+		else if (valinta == 2) { //nayta kaikki henkilot
 			TulostaKaikkiHenkilot(henkilot, henkilolkm);
 			cout << endl;
 		}
-		else if (valinta == 3) {
+		else if (valinta == 3) { //nayta yksi henkilo
 			TulostaHenkilo(henkilot[0]);
 		}
-		else if (valinta == 4) {
-
+		else if (valinta == 4) { //poista henkilon tiedot
+			cout << "Ei valmis\n\n";
 		}
-		else if (valinta == 5) {
-
+		else if (valinta == 5) { //tallenna tiedot tiedostoon
+			cout << "Ei valmis\n\n";
 		}
-		else if (valinta == 6) {
-
+		else if (valinta == 6) { //hae tiedot tiedostosta
+			cout << "Ei valmis\n\n";
 		}
 		else {
 			cout << "\n\nFATAL EXCEPTION HAS OCCURRED\n\n";
